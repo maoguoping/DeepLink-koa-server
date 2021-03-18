040739a3118fcc2b2bc015620948979368576dff
gpmao <673720904@qq.com>
2021-03-18 17:45:21 +0800

    :sparkle:feat:引入koa-helmet提升安全性

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import "reflect-metadata"
 import {createKoaServer} from "routing-controllers"
 import passport from "./passport"
+import helmet = require("koa-helmet") 
 import parameter = require('koa-parameter')
 import bodyParser = require('koa-bodyparser')
 const app = createKoaServer({
     controllers: [__dirname + "/controllers/*.ts"]
 });
+app.use(helmet())
 app.use(bodyParser()) 
 app.use(parameter(app));
 app.use(passport.initialize()) 
