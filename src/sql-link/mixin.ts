9de69fa7b47ca3b9d6d6edb4d402270d83ba1b44
gpmao <673720904@qq.com>
2021-04-18 22:02:59 +0800

    :art:refact拆解函数

--- src/sql-link/mixin.ts
+++ src/sql-link/mixin.ts
@@ -1,38 +1,38 @@
 import { Dispatch, SelectorDispatch } from './dispatch'
 import { Model } from './model';
+type KeyWithField = Record<string, string>
+function handleStaticData (staticData: any, keyWithField: KeyWithField): string[] {
+  let staticDataArr = Object.keys(staticData);
+  let arr:string [] = []
+  staticDataArr.length > 0 && staticDataArr.forEach(name => {
+    let item = staticData[name];
+    keyWithField[name] = name;
+    arr.push(`'${item.value}' AS ${name}`);
+  });
+  return arr
+}
+function handleDataArr(m: Model, dataArr: any, keyWithField: KeyWithField, spaceName: string): string[]{
+  let arr: string[] = []
+  dataArr.forEach((name: string) => {
+    let fullName = `${spaceName}.${m[name]}`;
+    keyWithField[name] = fullName;
+    arr.push(`${fullName} AS ${name}`);
+  });
+  return arr
+}
 export function mixin(m: Model, spaceName: string, selector: string | undefined | SelectorDispatch | any[]): Model {
   let _keyWithField: Record<string, string> = m._keyWithField;
   let _this = m;
-  function handleStaticData (staticData: any): string[] {
-    let staticDataArr = Object.keys(staticData);
-    let arr:string [] = []
-    staticDataArr.length > 0 && staticDataArr.forEach(name => {
-      let item = staticData[name];
-      _keyWithField[name] = name;
-      arr.push(`'${item.value}' AS ${name}`);
-    });
-    return arr
-  }
-  function handleDataArr(data:any, dataArr: any): string[]{
-    let arr: string[] = []
-    dataArr.forEach((name: string) => {
-      let item = data[name];
-      let fullName = `${spaceName}.${_this[name]}`;
-      _keyWithField[name] = fullName;
-      arr.push(`${fullName} AS ${name}`);
-    });
-    return arr
-  }
   if (selector == '' || selector == undefined) {
     let arr: string[] = [];
     let data = m.data;
     let staticData = m.staticData;
     let dataArr = Object.keys(data);
     if (dataArr.length > 0) {
-      arr = arr.concat(handleDataArr(data, dataArr))
+      arr = arr.concat(handleDataArr(m, dataArr, _keyWithField, spaceName))
     }
     if (staticData) {
-      arr = arr.concat(handleStaticData(staticData))
+      arr = arr.concat(handleStaticData(staticData, _keyWithField))
     }
     m.sqlSections.select = `SELECT ${arr.join(',')}`;
     m.attrStr = `${arr.join(',')}`;
@@ -47,7 +47,7 @@ export function mixin(m: Model, spaceName: string, selector: string | undefined 
         return !excludeList.includes(name);
       });
       if (dataArr.length > 0) {
-        arr = arr.concat(handleDataArr(data, dataArr))
+        arr = arr.concat(handleDataArr(m, dataArr, _keyWithField, spaceName))
       }
     } else if (selector.name === 'definition') {
       let definition = selector.reducer(m.context.Models[m._name], spaceName);
@@ -55,7 +55,7 @@ export function mixin(m: Model, spaceName: string, selector: string | undefined 
     }
 
     if (staticData) {
-      arr = arr.concat(handleStaticData(staticData))
+      arr = arr.concat(handleStaticData(staticData, _keyWithField))
     }
     m.sqlSections.select = `SELECT ${arr.join(',')}`;
     m.attrStr = `${arr.join(',')}`;
